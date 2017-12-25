f729fb5c98b8593086d48b81604cb90b7887b669
sunxiran <maricle01@163.com>
2017-12-25 21:01:07 +0800

    feature 添加敌人移动

--- js/app.js
+++ js/app.js
@@ -1,51 +1,103 @@
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+var Enemy = function () {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
+    this.active = false;
+    this.x = 0;
+    this.y = 0;
+    this.duaration = 0.7;
+    this.speed = 0;
 
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = "images/enemy-bug.png";
 };
 
+Enemy.prototype.toActive = function () {
+    this.active = true;
+    var row = parseInt(Math.random() * 3 + 1, 10);
+    this.y = row * staticDimension.IMAGE_HEIGHT - 10;
+    this.speed = parseInt(Math.random() * 2 + 1, 10) * 300;
+};
+
+Enemy.prototype.toNegative = function () {
+    this.active = false;
+    this.duaration = 0;
+    this.x = (0 - staticDimension.IMAGE_WIDTH);
+};
+
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
+    if (this.active) {
+        this.x = this.x + dt * this.speed;
+        if (this.x >= staticDimension.GAME_WIDTH) {
+            this.toNegative();
+        }
+    } else {
+        this.duaration += dt;
+        if (this.duaration > 0.8) {
+            this.toActive();
+        }
+    }
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+    if (this.active) {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
-var Player = function() {
+var Player = function () {
     this.x = 10;
     this.y = 10;
     this.sprite = "images/char-horn-girl.png";
 };
 
-Player.prototype.update = function(dt) {
+Player.prototype.update = function (dt) {
 
 };
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function () {
+
 };
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 
+var staticDimension = {
+    IMAGE_WIDTH: 101,
+    IMAGE_HEIGHT: 83,
+    GAME_WIDTH: 101 * 5,
+    GAME_HEIGHT: 83 * 6
+};
+
 var allEnemies = [];
+
 var player = new Player();
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
-
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
